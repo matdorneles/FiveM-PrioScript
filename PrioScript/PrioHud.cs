799693a6288407c7642cb4741174df004303a0b6
Mxdden01 <125059932+Mxdden01@users.noreply.github.com>
2023-04-02 22:13:03 +0100

    Updated Hud

--- PrioScript/PrioHud.cs
+++ PrioScript/PrioHud.cs
@@ -159,24 +159,24 @@ namespace PrioScript
         public static void DrawText()
         {
             API.SetTextFont(0);
-            API.SetTextScale(0.0f, 0.3f);
+            API.SetTextScale(0.0f, 0.26f);
             API.SetTextColour(128, 128, 128, 255);
             API.SetTextDropshadow(0, 0, 0, 0, 255);
             API.SetTextDropShadow();
             API.SetTextOutline();
             API.SetTextEntry("STRING");
             API.AddTextComponentString($"~w~~h~County Priority: {countyHud}");
-            API.DrawText(0.168f, 0.8520f);
+            API.DrawText(0.168f, 0.8799f);
 
             API.SetTextFont(0);
-            API.SetTextScale(0.0f, 0.3f);
+            API.SetTextScale(0.0f, 0.26f);
             API.SetTextColour(128, 128, 128, 255);
             API.SetTextDropshadow(0, 0, 0, 0, 255);
             API.SetTextDropShadow();
             API.SetTextOutline();
             API.SetTextEntry("STRING");
             API.AddTextComponentString($"~w~~h~City Priority: {cityHud}");
-            API.DrawText(0.168f, 0.8725f);
+            API.DrawText(0.168f, 0.8970f);
         }
     }
 }
